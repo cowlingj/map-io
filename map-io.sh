7870ad8387231a03b07b156d60e5b811d56c6162
cowlingj <09cowlingj@gmail.com>
2021-05-02 23:52:52 +0100

    Add a way to reset the mapping

--- map-io.sh
+++ map-io.sh
@@ -1,12 +1,17 @@
 #!/usr/bin/env bash
 
+function reset_mapping {
+  DEVICE="$(awk '{print $1}' <<< "$1")"
+  xinput set-prop "$DEVICE" 'Coordinate Transformation Matrix' 1.0 0.0 0.0 0.0 1.0 0.0 0.0 0.0 1.0 > test.txt
+}
+
 function map_io {
   DEVICE="$(awk '{print $1}' <<< "$1")"
   MONITOR="$2"
   xinput map-to-output "$DEVICE" "$MONITOR"
 }
 
-export -f map_io
+export -f map_io reset_mapping
 
 yad \
   --form \
@@ -21,4 +26,5 @@ yad \
     'Output:':CB \
     "$(xrandr --listmonitors | awk -e 'NR != 1 { print $NF }' | sort | tr '\n' '!' | head -c -1)"  \
   --field "Apply!gtk-ok:BTN" 'bash -c "map_io %1 %2"' \
+  --field "Reset Input Device:BTN" 'bash -c "reset_mapping %1' \
   --button "Close" "0" > /dev/null
